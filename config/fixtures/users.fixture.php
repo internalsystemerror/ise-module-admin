f847ea344d0401b3db02df1b5cc22aeea3d81e2d
ise <iam@ise.me.uk>
2016-12-30 19:14:21 +0000

    Modify roles

--- config/fixtures/users.fixture.php
+++ config/fixtures/users.fixture.php
@@ -1,12 +1,24 @@
 <?php
 return [
-    'admin'  => [
+    'admin_demo'  => [
         'role'         => 'admin',
         'email'        => 'admin@example.com',
         'display_name' => 'Admin User',
         'password'     => '$2y$14$zMHbytWzy0voD.0Ppzbmm.KbUHFSMsNycR43nORg0mKUb4ZgKxwGK',
     ],
-    'test' => [
+    'user_admin_demo' => [
+        'role'         => 'user_admin',
+        'email'        => 'admin@example.com',
+        'display_name' => 'User Admin User',
+        'password'     => '$2y$14$zMHbytWzy0voD.0Ppzbmm.KbUHFSMsNycR43nORg0mKUb4ZgKxwGK',
+    ],
+    'rbac_admin_demo' => [
+        'role'         => 'rbac_admin',
+        'email'        => 'admin@example.com',
+        'display_name' => 'RBAC Admin User',
+        'password'     => '$2y$14$zMHbytWzy0voD.0Ppzbmm.KbUHFSMsNycR43nORg0mKUb4ZgKxwGK',
+    ],
+    'member_demo' => [
         'role'         => 'member',
         'email'        => 'test@example.com',
         'display_name' => 'Test User',
