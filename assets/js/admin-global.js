63cdb12c6e604ffaf7473f3a3f2e8360a6247ed3
ise <iam@ise.me.uk>
2017-01-06 21:30:17 +0000

    Change collapsible

--- assets/js/admin-global.js
+++ assets/js/admin-global.js
@@ -20,11 +20,38 @@
                     $(window).load(methods.loaded);
                     $('.table').dataTable();
                     $('.modal').on('hide.bs.modal', methods.hideModal);
+                    $('[data-toggle="collapse"]').on('click', methods.collapse).each(methods.prepareCollapse);
                     $('.alert-notifications .alert').each(methods.showNotification);
                 },
                 loaded: function ()
                 {
                     $('body').addClass('loaded');
+                },
+                prepareCollapse: function()
+                {
+                    var $this = $(this), target = $($this.attr('data-target'));
+                    if (target.hasClass('in')) {
+                        $this.data('originalTitle', $this.html());
+                        $this.html('<span class="glyphicon glyphicon-minus" aria-hidden="true" /> Hide');
+                    }
+                },
+                collapse: function ()
+                {
+                    var $this = $(this), target = $($this.attr('data-target'));
+                    if (target.hasClass('collapsing')) {
+                        return;
+                    }
+                    
+                    var originalTitle = $this.data('originalTitle');
+                    if (originalTitle && target.hasClass('in')) {
+                        $this.html(originalTitle);
+                        $this.data('originalTitle', null);
+                        return;
+                    }
+                    if (!originalTitle) {
+                        $this.data('originalTitle', $this.html());
+                        $this.html('<span class="glyphicon glyphicon-minus" aria-hidden="true" /> Hide');
+                    }
                 },
                 hideModal: function ()
                 {
