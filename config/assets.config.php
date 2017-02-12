ab8a6814b3bf4f9c81fa2264d6244f425339aae8
ise <iam@ise.me.uk>
2017-02-12 13:10:54 +0000

    Add responsive datatables

--- config/assets.config.php
+++ config/assets.config.php
@@ -6,11 +6,13 @@ return [
         'collections' => [
             'css/admin.css' => [
                 'css/datatables-bootstrap.css',
+                'css/datatables-responsive.css',
                 'css/admin-global.css',
                 'css/admin-queries.css',
             ],
             'js/admin.js'   => [
                 'js/datatables.js',
+                'js/datatables-responsive.js',
                 'js/datatables-bootstrap.js',
                 'js/admin-global.js',
             ],
@@ -26,13 +28,14 @@ return [
             'images/sort_desc.png'          => 'http://cdn.datatables.net/plug-ins/' . $dataTablesVersion . '/integration/bootstrap/images/sort_desc.png',
             'images/sort_desc_disabled.png' => 'http://cdn.datatables.net/plug-ins/' . $dataTablesVersion . '/integration/bootstrap/images/sort_desc_disabled.png',
             /* CSS */
-            'css/datatables.css'            => 'http://cdn.datatables.net/' . $dataTablesVersion . '/css/jquery.dataTables.css',
             'css/datatables-bootstrap.css'  => 'http://cdn.datatables.net/plug-ins/' . $dataTablesVersion . '/integration/bootstrap/3/dataTables.bootstrap.css',
+            'css/datatables-responsive.css' => 'https://cdn.datatables.net/responsive/2.1.1/css/responsive.bootstrap.min.css',
             'css/admin-global.css'          => __DIR__ . '/../assets/css/admin-global.css',
             'css/admin-queries.css'         => __DIR__ . '/../assets/css/admin-queries.css',
             'css/login.css'                 => __DIR__ . '/../assets/css/login.css',
             /* JS */
             'js/datatables.js'              => 'http://cdn.datatables.net/' . $dataTablesVersion . '/js/jquery.dataTables.js',
+            'js/datatables-responsive.js'   => 'https://cdn.datatables.net/responsive/2.1.1/js/dataTables.responsive.js',
             'js/datatables-bootstrap.js'    => 'http://cdn.datatables.net/plug-ins/' . $dataTablesVersion . '/integration/bootstrap/3/dataTables.bootstrap.js',
             'js/admin-global.js'            => __DIR__ . '/../assets/js/admin-global.js',
             'js/login.js'                   => __DIR__ . '/../assets/js/login.js',
